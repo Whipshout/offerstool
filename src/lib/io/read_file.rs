560222f62989f4d5240dfc0b43e432749d5f1045
Eduardo Sánchez <whipshout@gmail.com>
2021-11-03 18:00:29 +0100

    Removed comments

--- src/lib/io/read_file.rs
+++ src/lib/io/read_file.rs
@@ -9,15 +9,8 @@ pub fn read_file(path: String) -> Result<String, std::io::Error> {
 
     let mut file = File::open(&path)?;
     let file_len = file.metadata().unwrap().len();
-
-    // let mut file_buf_reader = BufReader::new(file);
-    // let mut file_contents = String::with_capacity(file_len as usize + 1);
-    // file_buf_reader.read_to_string(&mut file_contents)?;
     let mut file_contents = Vec::with_capacity(file_len as usize + 1);
     file.read_to_end(&mut file_contents)?;
-
-    // let mut contents_string = String::with_capacity(file_len as usize + 1);
-    // file.read_to_string(&mut contents_string)?;
 
     let elapsed = now.elapsed().as_secs_f64();
 
